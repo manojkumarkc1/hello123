288445090ff445fec77e2b98cc56534a61e2ba0f
manojkumarkc1 <manojfer987@gmail.com>
2019-12-04 13:32:20 +0545

    second

--- src/Hello.java
+++ src/Hello.java
@@ -14,6 +14,8 @@ public class Hello {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
+        System.out.println("manoj");
+        
         System.out.println("hello");
         
         // TODO code application logic here
